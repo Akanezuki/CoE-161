01b7823f3b50c4ab583e1c08fe06a3b2d9f67c56
Michael Medrano <michael.medrano@eee.upd.edu.ph>
2021-03-23 10:41:19 +0800

    v1.1 for submission
    removed header and test cases to make it ready in submission format.
    
    first submission

--- A2-2.py
+++ A2-2.py
@@ -1,41 +1,3 @@
-class Node:
-    def __init__(self, symbol=None):
-        self.left = None
-        self.right = None
-        self.data = symbol
-
-    def __del__(self):
-        if self.left is not None:
-            del self.left
-        if self.right is not None:
-            del self.right
-        del self.data
-    
-    def insert(self, bitstring, symbol):
-        if bitstring == '':
-            self.data = symbol
-        elif bitstring[0] == '0':
-            if self.left is None:
-                self.left = Node()
-            
-            self.left.insert(bitstring[1:], symbol)
-        elif bitstring[0] == '1':
-            if self.right is None:
-                self.right = Node()
-            
-            self.right.insert(bitstring[1:], symbol)
-
-class Decoder:
-    def __init__(self, symbol_map):
-        self.root = Node()
-        for (bit_string, symbol) in symbol_map.items():
-            self.root.insert(bit_string, symbol)
-    
-    def __del__(self):
-        del self.root
-
-#-----------------------------------END--OF--HEADER----------------------------------#
-
 def prefix_free_decode(decoder, bit_string):
     bs_index = 0
     tlist = []
@@ -61,11 +23,6 @@ def prefix_free_decode(decoder, bit_string):
     ans = ''.join(slist)
 
     return ans
-
-x = prefix_free_decode(Decoder({'10010':'p','10011':'t','0100':'d', '1110':'i', '000':'n', '10100':'y', '0101':'c', '10101':'u', '0110':'s', '10110':'h', '0111':'r', '110':' ', '001':'e', '10111':'v', '1111':'a', '1000':'g'}), '111100001001100011011100101111010010011101101110000100011000011100101001')
-print(x)
-y = prefix_free_decode(Decoder({'00':'e','01':'h','10':'y', '11':' '}), '0100101101001011010010')
-print(y)
 
 
 
