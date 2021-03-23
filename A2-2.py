1b2caceaa1a33821515b63f3c39fb0a574e3f27b
Michael Medrano <michael.medrano@eee.upd.edu.ph>
2021-03-23 10:38:40 +0800

    v1.0 of prefix_free_decode
    -created nested function data_pop_r
    -passed given test cases

--- A2-2.py
+++ A2-2.py
@@ -37,11 +37,40 @@ class Decoder:
 #-----------------------------------END--OF--HEADER----------------------------------#
 
 def prefix_free_decode(decoder, bit_string):
-    # your code..
-    # more code..
+    bs_index = 0
+    tlist = []
+    slist = []
+    for x in range(len(bit_string)):
+        tlist.append(bit_string[x])
+    
+    def data_pop_r(Node, bitlist, string_list):
+        if Node.data != None:
+            string_list.append(Node.data)
+            return Node.data
+        
+        b = bitlist.pop(0)
+
+        if b == '0':
+            data_pop_r(Node.left, bitlist, string_list)
+        if b == '1':
+            data_pop_r(Node.right, bitlist, string_list)
+
+    while len(tlist) != 0:
+        data_pop_r(decoder.root, tlist, slist)
+
+    ans = ''.join(slist)
 
     return ans
-}
+
+x = prefix_free_decode(Decoder({'10010':'p','10011':'t','0100':'d', '1110':'i', '000':'n', '10100':'y', '0101':'c', '10101':'u', '0110':'s', '10110':'h', '0111':'r', '110':' ', '001':'e', '10111':'v', '1111':'a', '1000':'g'}), '111100001001100011011100101111010010011101101110000100011000011100101001')
+print(x)
+y = prefix_free_decode(Decoder({'00':'e','01':'h','10':'y', '11':' '}), '0100101101001011010010')
+print(y)
+
+
+
+
+
 
 
 
